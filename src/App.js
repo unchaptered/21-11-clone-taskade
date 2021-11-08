772467eff0c560aad5a36d6824c9394d753c3266
Unchaptered <workstation19961002@gmail.com>
2021-11-08 20:06:52 +0900

    App published

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <span> hi </span>;
+import React, { Components } from "react";
+import InitStyles from "./Components/InitStyles";
+import Router from "./Router";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Router />
+        <InitStyles />
+      </>
+    );
+  }
 }
 
 export default App;
