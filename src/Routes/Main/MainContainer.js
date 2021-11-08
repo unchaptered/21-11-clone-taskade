883b6d5fb755caf042a5ebc33596d2aae93ba01f
Unchaptered <workstation19961002@gmail.com>
2021-11-08 20:08:41 +0900

    Main Route Published

---
+++ src/Routes/Main/MainContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import MainPresenter from "./MainPresenter";
+
+export default class extends React.Component {
+  render() {
+    return <MainPresenter />;
+  }
+}
